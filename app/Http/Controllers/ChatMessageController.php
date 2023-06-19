28ad294bb3fb1c7ecc4a7724c6a2a7495ed095a1
lsestrada <leonard.estrada@ecashpay.com.ph>
2023-06-19 19:07:41 +0800

    added sender field to ChatMessageController

--- app/Http/Controllers/ChatMessageController.php
+++ app/Http/Controllers/ChatMessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ChatMessage;
 use Illuminate\Http\Request;
 use Throwable;
+use DB;
 
 class ChatMessageController extends Controller
 {
@@ -26,12 +27,15 @@ class ChatMessageController extends Controller
 
         try {
 
-           
 
-            return ChatMessage::leftJoin('users as sender_csr', 'sender_csr.id', '=',  'sender_id')
-                                ->leftJoin('users as receiver_csr', 'receiver_csr.id', '=', 'receiver_id')
-                                ->select("sender_csr.full_name as sender_csr", "receiver_csr.full_name as receiver_csr", "chat_messages.*")
-                                ->where('chat_room_id', $room_id)->get();
+
+            return ChatMessage::leftJoin('users as sender_csr', 'sender_csr.id', '=', 'sender_id')
+                ->leftJoin('users as receiver_csr', 'receiver_csr.id', '=', 'receiver_id')
+                ->leftJoin('callers as caller_sender', 'caller_sender.id', '=', 'sender_id')
+                ->leftJoin('callers as caller_receiver', 'caller_receiver.id', '=', 'receiver_id')
+                ->select("sender_csr.full_name as sender_csr", "receiver_csr.full_name as receiver_csr", "caller_receiver.firstname as creceiver", "caller_sender.firstname as csender",
+                DB::raw("IFNULL(sender_csr.full_name, caller_sender.firstname) as sender"), "chat_messages.*" )
+                ->where('chat_room_id', $room_id)->get();
         } catch (Throwable $error) {
             return $error;
         }
